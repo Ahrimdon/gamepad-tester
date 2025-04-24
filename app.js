ae9710d7241da5c43baaf4a0345bd16eb97e7c4d
Rim <info@rimmyscorner.com>
2025-04-23 20:40:48 -0400

    format

--- app.js
+++ app.js
@@ -7,21 +7,20 @@ const url = require("url");
 const port = 8080;
 const host = "0.0.0.0";
 const mimeTypes = {
-  ".html": "text/html",
-  ".css": "text/css",
-  ".js": "application/javascript",
-  ".svg": "image/svg+xml",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".jpeg": "image/jpeg",
-  ".gif": "image/gif",
-  ".ico": "image/x-icon",
-  ".json": "application/json",
-  ".txt": "text/plain",
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".svg": "image/svg+xml",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".gif": "image/gif",
+    ".ico": "image/x-icon",
+    ".json": "application/json",
+    ".txt": "text/plain",
 };
 
-http
-  .createServer((req, res) => {
+http.createServer((req, res) => {
     // Parse the URL and extract just the pathname, ignoring query parameters
     const parsedUrl = url.parse(req.url);
     let filePath = "." + decodeURIComponent(parsedUrl.pathname);
@@ -31,15 +30,14 @@ http
     const contentType = mimeTypes[ext] || "application/octet-stream";
 
     fs.readFile(filePath, (err, content) => {
-      if (err) {
-        res.writeHead(404);
-        res.end("404 Not Found");
-      } else {
-        res.writeHead(200, { "Content-Type": contentType });
-        res.end(content);
-      }
+        if (err) {
+            res.writeHead(404);
+            res.end("404 Not Found");
+        } else {
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content);
+        }
     });
-  })
-  .listen(port, host, () => {
+}).listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`);
-  });
+});
